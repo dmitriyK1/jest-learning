cd2717f7668a14db8b7e108ba1f6b25b8cf91abe
Dmitriy Kaminskiy <dimaroxx@gmail.com>
2018-02-25 21:03:09 +0200

    update snapshot

--- src/App.test.js
+++ src/App.test.js
@@ -3,17 +3,14 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import render from 'react-test-renderer';
 
-describe('App component tests', () => {
-    it('renders without crashing', () => {
-        const div = document.createElement('div');
-        ReactDOM.render(<App />, div);
-        ReactDOM.unmountComponentAtNode(div);
-    });
-
-    it('snapshot matches to stored snapshot', () => {
-        const tree = render.create(<App />).toJSON();
-
-        expect(tree).toMatchSnapshot();
-    });
+it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
 });
 
+it('snapshot matches to stored snapshot', () => {
+    const tree = render.create(<App />).toJSON();
+
+    expect(tree).toMatchSnapshot();
+});
