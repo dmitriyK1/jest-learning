64de9e31c0026bdfbd9d8b0822da925db5d25c3c
Dmitriy Kaminskiy <dimaroxx@gmail.com>
2018-02-25 18:56:00 +0200

    add App snapshot

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import jest from 'jest';
+import render from 'react-test-renderer';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+
+  const temp = render.create(<App />).toJSON();
+    console.log(temp);
+
+  expect(temp).toMatchSnapshot();
 });
