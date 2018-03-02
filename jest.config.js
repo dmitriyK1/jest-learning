f4a4e83c88695034d2426a2e09e40c37e9a69e1d
Dmitriy Kaminskiy <dimitriy.kaminskiy@nextiva.com>
2018-03-03 01:13:02 +0200

    add enzyme

--- jest.config.js
+++ jest.config.js
@@ -3,8 +3,9 @@ module.exports = {
   testEnvironment: 'jsdom',
   moduleNameMapper: {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
-      "<rootDir>/__mocks__/fileMock.js",
-    "\\.(css|less)$": "<rootDir>/__mocks__/styleMock.js"
+      "<rootDir>/jest/__mocks__/fileMock.js",
+    "\\.(css|less)$": "<rootDir>/jest/__mocks__/styleMock.js"
   },
   globals: {}, // used for mocking global variables
+  setupFiles: ['<rootDir>/jest/setupFiles/enzyme.js']
 };
