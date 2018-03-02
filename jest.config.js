17eaa8cfe9398cd2d6a1d3e9d9ccce7e243bd667
Dmitriy Kaminskiy <dimitriy.kaminskiy@nextiva.com>
2018-03-03 01:34:17 +0200

    update jest config

--- jest.config.js
+++ jest.config.js
@@ -4,8 +4,10 @@ module.exports = {
   moduleNameMapper: {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
       "<rootDir>/jest/__mocks__/fileMock.js",
-    "\\.(css|less)$": "<rootDir>/jest/__mocks__/styleMock.js"
+    // "\\.(css|less)$": "<rootDir>/jest/__mocks__/styleMock.js",
+    '\\.(css|scss)$': 'identity-obj-proxy'
   },
   globals: {}, // used for mocking global variables
-  setupFiles: ['<rootDir>/jest/setupFiles/enzyme.js']
+  setupFiles: ['<rootDir>/jest/setupFiles/enzyme.js'],
+  snapshotSerializers: ['enzyme-to-json/serializer'],
 };
