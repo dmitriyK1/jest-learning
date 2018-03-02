0a1d0e673b07158412d3ebe1f802571216a6a723
Dmitriy Kaminskiy <dimitriy.kaminskiy@nextiva.com>
2018-03-03 00:37:22 +0200

    add basic jest config

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('my tests', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('says hi', () => {
+    console.log('hi');
+  });
 });
+
