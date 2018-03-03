062a948db19865f53a2294ca707260e3be358d9a
Dmitriy Kaminskiy <dimitriy.kaminskiy@nextiva.com>
2018-03-03 02:16:11 +0200

    update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   verbose: true,
   testEnvironment: 'jsdom',
+  testURL: 'http://localhost', // This option sets the URL for the jsdom environment. It is reflected in properties such as location.href.
   moduleNameMapper: {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
       "<rootDir>/jest/__mocks__/fileMock.js",
