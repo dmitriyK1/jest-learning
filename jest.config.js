4941d4c7565d645ad8811acaa6b816fe08d7132e
Dmitriy Kaminskiy <dimaroxx@gmail.com>
2018-04-24 22:48:43 +0300

    update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,26 @@
 module.exports = {
-  "setupTestFrameworkScriptFile": "<rootDir>/jest.setup.js",
-  "moduleNameMapper": {
-        "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
-            "<rootDir>/__mocks__/fileMock.js",
-        "\\.(css|less)$": "<rootDir>/__mocks__/styleMock.js"
-    }
+  cache: false,
+  testMatch: [
+    '<rootDir>/src/**/__tests__/**/*.{js,jsx,mjs}',
+    '<rootDir>/src/**/?(*.)(spec|test).{js,jsx,mjs}',
+  ],
+  testEnvironment: 'node',
+  testURL: 'http://localhost',
+  "setupTestFrameworkScriptFile": "<rootDir>/src/setupTests.js",
+  setupFiles: ['<rootDir>/__mocks__/polyfills.js'],
+  "transform": {
+    '^(?!.*\\.(js|jsx|mjs|css|json)$)': "<rootDir>/__mocks__/fileTransform.js",
+    '^.+\\.(js|jsx|mjs)$': '<rootDir>/__mocks__/babelTransform.js',
+    '^.+\\.css$': '<rootDir>/__mocks__/cssTransform.js',
+  },
+  moduleFileExtensions: [
+    'web.js',
+    'mjs',
+    'js',
+    'json',
+    'web.jsx',
+    'jsx',
+    'node',
+  ],
+  transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs)$'],
 };
